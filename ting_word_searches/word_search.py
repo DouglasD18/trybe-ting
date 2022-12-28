b06db67fa453dbeca42cb37b005ecd8554387d3c
Douglinhas <daguiaralcantara@gmail.com>
2022-12-28 16:44:58 -0300

    Implementando o método exists_word do módulo word_search.

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,26 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
+
+
+def exists_word(word, instance: Queue()):
+    lower = word.lower()
+    length = len(instance)
+    lista = []
+    for index in range(length):
+        data = instance.search(index)
+        lines = data["linhas_do_arquivo"]
+        lines_number = []
+        for number in range(len(lines)):
+            if lower in lines[number].lower():
+                lines_number.append(number + 1)
+
+        if len(lines_number):
+            lista.append({
+                "palavra": word,
+                "arquivo": data["nome_do_arquivo"],
+                "ocorrencias": [{"linha": number} for number in lines_number]
+            })
+
+    return lista
 
 
 def search_by_word(word, instance):
