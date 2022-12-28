3f6a232a5a46459303861e7df5717c25beba865d
Douglinhas <daguiaralcantara@gmail.com>
2022-12-28 15:26:41 -0300

    Implemtando a função txt_importer do módulo file_management.

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if '.txt' not in path_file:
+            return print('Formato inválido', file=sys.stderr)
+
+        with open(path_file, encoding="utf8") as file:
+            data = file.read()
+            lines = [line for line in data.split("\n")]
+            return lines
+
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
