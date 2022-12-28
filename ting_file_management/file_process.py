e63bb3f37e65d643ba3934e6762dd7c54c53aaf3
Douglinhas <daguiaralcantara@gmail.com>
2022-12-28 15:51:18 -0300

    Implementando o método process do módulo file_process.

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.file_management import txt_importer
+from ting_file_management.queue import Queue
+import sys
+
+
+def create_dict(path, line, instance: Queue()):
+    line_dict = {
+        "nome_do_arquivo": path,
+        "qtd_linhas": len(line),
+        "linhas_do_arquivo": line,
+    }
+    instance.enqueue(line_dict)
+    print(line_dict, file=sys.stdout)
+
+
+def process(path_file, instance: Queue()):
+    line = txt_importer(path_file)
+    if len(instance) == 0:
+        create_dict(path_file, line, instance)
 
 
 def remove(instance):
