508a2d5c24ce41001641dedd4705ca0487a21a36
Douglinhas <daguiaralcantara@gmail.com>
2022-12-28 15:57:23 -0300

    Implementando o método remove do módulo file_process.

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -19,8 +19,13 @@ def process(path_file, instance: Queue()):
         create_dict(path_file, line, instance)
 
 
-def remove(instance):
-    """Aqui irá sua implementação"""
+def remove(instance: Queue()):
+    if len(instance) > 0:
+        removed = instance.dequeue()
+        file = removed["nome_do_arquivo"]
+        print(f"Arquivo {file} removido com sucesso", file=sys.stdout)
+
+    print("Não há elementos", file=sys.stdout)
 
 
 def file_metadata(instance, position):
